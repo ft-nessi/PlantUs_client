dd521304531527e471dd8b4faa00cf6d4524d0b9
Lysann Kuettner <ly.sann@live.de>
2022-04-09 13:15:35 +0200

    Added Marker for map

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ import { Route, Routes } from "react-router-dom";
 import { Home } from "./components/Home";
 import { LayoutComponent } from "./components/LayoutComponent";
 import { Login } from "./components/Login";
-import { Profile } from "./components/Profile";
+// import { Profile } from "./components/Profile";
 import { Signup } from "./components/Signup";
+import { Ranger } from "./components/Ranger";
 
 export default function App() {
   useEffect(() => {
@@ -19,7 +20,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile" element={<Ranger />} />
           {/* <Route path="/profile/markedTrees" element={} />
           <Route path="/profile/treedetails" element={} />
           <Route path="/profile/plantedTrees" element={} />
