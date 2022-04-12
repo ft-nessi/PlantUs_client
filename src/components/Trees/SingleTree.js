053340993efbd55a541a2213bda60a6da5dc2a49
Lysann Kuettner <ly.sann@live.de>
2022-04-12 16:33:08 +0200

    Edit tree form

--- src/components/Trees/SingleTree.js
+++ src/components/Trees/SingleTree.js
@@ -45,7 +45,7 @@ export function SingleTree({ tree, updateSingleTree, deleteSingleTree, errorStat
             location: {tree.location.coordinatesX},{tree.location.coordinatesY}
           </p>
           <button onClick={handleEdit}>Edit</button>
-          {!user.isUser && (<button onClick={handleDeleteTree}>Delete!</button>)}
+          {!user.isUser && <button onClick={handleDeleteTree}>Delete!</button>}
         </div>
       )}
       {isEditing && !user.isUser && (
@@ -54,16 +54,20 @@ export function SingleTree({ tree, updateSingleTree, deleteSingleTree, errorStat
             <h2>Name:{tree.treename}</h2>
             <p>
               Possible kinds:{" "}
-              <input type="text" name="kind" onChange={handleChange} value={formEdit.kind}/>
+              <input
+                type="text"
+                name="kind"
+                onChange={handleChange}
+                value={formEdit.kind}
+              />
             </p>
             <p>
-              location:
-              [<label htmlFor="coordinatesX">X: </label>
-                <input
+              location: [<label htmlFor="coordinatesX">X: </label>
+              <input
                 type="number"
                 id="coordinatesX"
                 name="coordinatesX"
-                value={formEdit.location.coordinatesX} 
+                value={formEdit.location.coordinatesX}
                 onChange={handleChange}
               />
               , <label htmlFor="coordinatesY">Y: </label>
@@ -71,10 +75,12 @@ export function SingleTree({ tree, updateSingleTree, deleteSingleTree, errorStat
                 type="number"
                 name="coordinatesY"
                 id="coordinatesY"
-                value={formEdit.location.coordinatesY} 
+                value={formEdit.location.coordinatesY}
                 onChange={handleChange}
-              />]
+              />
+              ]
             </p>
+
             <button type="submit">Save</button>
             {errorState && errorState}
           </div>
@@ -82,13 +88,29 @@ export function SingleTree({ tree, updateSingleTree, deleteSingleTree, errorStat
       )}
       {isEditing && user.isUser && (
         <form onSubmit={handleUpdateTree}>
-          <div style={{ backgroundColor: "green"}}>
-            <h2>Name:<input type="text" name="treename" onChange={handleChange} value={formEdit.treename}/></h2>
+          <div style={{ backgroundColor: "green" }}>
+            <h2>
+              Name:
+              <input
+                type="text"
+                name="treename"
+                onChange={handleChange}
+                value={formEdit.treename}
+              />
+            </h2>
+            <p>Possible kinds:{formEdit.kind}</p>
             <p>
-              Possible kinds:{formEdit.kind}
+              location: [{formEdit.location.coordinatesX},
+              {formEdit.location.coordinatesY}]
             </p>
             <p>
-              location: [{formEdit.location.coordinatesX}, {formEdit.location.coordinatesY}]
+              planted date:
+              <input
+                type="date"
+                name="plantedDate"
+                onChange={handleChange}
+                value={formEdit.plantedDate}
+              />
             </p>
             <button type="submit">Save</button>
             {errorState && errorState}
--- src/components/Trees/index.js
+++ src/components/Trees/index.js
@@ -8,8 +8,7 @@ import { SingleTree } from "./SingleTree";
 
 export function Trees() {
   const [allTrees, setAllTrees] = useState([]);
-  const [errorState, setErrorState] = useState()
-
+  const [errorState, setErrorState] = useState();
 
   const navigate = useNavigate();
   const user = useContext(AuthContext);
@@ -25,7 +24,9 @@ export function Trees() {
     async function fetchAllTrees() {
       try {
         if (!user.isUser) {
-          const { data } = await axios.get(`${API_BASE_URL}/ranger/markedtrees`);
+          const { data } = await axios.get(
+            `${API_BASE_URL}/ranger/markedtrees`
+          );
           console.log(data);
           if (!data.trees) return;
           setAllTrees(data.trees);
@@ -44,30 +45,66 @@ export function Trees() {
     fetchAllTrees();
   }, [navigate]);
 
-  // router.put /ranger/markedtrees - find a tree with a specific id and update it
   const updateSingleTree = async (idToUpdate, updatedTree) => {
     try {
-      const response = await axios.put(
-        `${API_BASE_URL}/ranger/markedtrees`,
-        updatedTree
-      );
-      console.log(response.data);
-      setAllTrees((oldTrees) => {
-        return oldTrees.map((tree) => {
-          if (idToUpdate === tree._id) {
-            return updatedTree;
-          }
-          return tree;
+      if (!user.isUser) {
+        const response = await axios.put(
+          `${API_BASE_URL}/ranger/markedtrees`,
+          updatedTree
+        );
+        console.log("response data", response.data);
+        setAllTrees((oldTrees) => {
+          return oldTrees.map((tree) => {
+            if (idToUpdate === tree._id) {
+              return updatedTree;
+            }
+            return tree;
+          });
         });
-      });
+      } else if (user.isUser) {
+        const response = await axios.put(
+          `${API_BASE_URL}/owner/mytrees`,
+          updatedTree
+        );
+        console.log("response data", response.data);
+        setAllTrees((oldTrees) => {
+          return oldTrees.map((tree) => {
+            if (idToUpdate === tree._id) {
+              return updatedTree;
+            }
+            return tree;
+          });
+        });
+      }
     } catch (err) {
-      setErrorState(err)
+      setErrorState(err);
       console.log("Error in updating the tree on the server", err);
     }
   };
+  // router.put /ranger/markedtrees - find a tree with a specific id and update it
+  // const updateSingleTree = async (idToUpdate, updatedTree) => {
+  //   try {
+  //     const response = await axios.put(
+  //       `${API_BASE_URL}/ranger/markedtrees`,
+  //       updatedTree
+  //     );
+  //     console.log(response.data);
+  //     setAllTrees((oldTrees) => {
+  //       return oldTrees.map((tree) => {
+  //         if (idToUpdate === tree._id) {
+  //           return updatedTree;
+  //         }
+  //         return tree;
+  //       });
+  //     });
+  //   } catch (err) {
+  //     setErrorState(err)
+  //     console.log("Error in updating the tree on the server", err);
+  //   }
+  // };
   // router.delete /ranger/markedtrees find a tree with that id and delete it
   const deleteSingleTree = async (idToDelete) => {
-    console.log(idToDelete)
+    console.log(idToDelete);
     try {
       const response = await axios.post(
         `${API_BASE_URL}/ranger/markedtrees/delete`,
@@ -89,16 +126,15 @@ export function Trees() {
       {/* <AddNewTree setAllTrees={setAllTrees} /> */}
       {allTrees.map((tree) => (
         <div>
-        <SingleTree
-          key={tree._id}
-          tree={tree}
-          updateSingleTree={updateSingleTree}
-          deleteSingleTree={deleteSingleTree}
-          errorState={errorState}
-        />
+          <SingleTree
+            key={tree._id}
+            tree={tree}
+            updateSingleTree={updateSingleTree}
+            deleteSingleTree={deleteSingleTree}
+            errorState={errorState}
+          />
         </div>
       ))}
-
     </div>
   );
 }
